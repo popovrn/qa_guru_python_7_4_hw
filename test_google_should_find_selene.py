ae51d9f7c200ed8c73b47be4d120669cde28980f
Roman Popov <rpopov2014@gmail.com>
2023-09-26 11:56:54 +0300

    delete "Hi"

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -4,4 +4,4 @@ from selene import be, have
 
 browser.open('https://google.com')
 browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-browser.element('[id="search"]').should(have.text('Hi User-oriented Web UI browser tests in Python'))
+browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
